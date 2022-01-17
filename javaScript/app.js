31b932b823d30d78b532d194657854c05695d2da
origuy <92257911+origuy@users.noreply.github.com>
2022-01-17 19:21:47 +0200

    Update app.js

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,7 +1,17 @@
 const overlayTapDisplay = document.getElementById('overlayHover');
+const mainHeader = document.getElementById('mainHeader');
+const mainHeader = document.getElementById('developerName');
 
 overlayTapDisplay.addEventListener('touchstart', showOverlay)
+mainHeader.addEventListener('touchstart', changeColorHeader);
+developerName.addEventListener('touchstart', changeColorDeveloperName);
 
 function showOverlay(){
     overlayTapDisplay.style.opacity = '1';
+}
+function changeColorHeader(){
+    mainHeader.style.color = '#00fff7';
+}
+function changeColorDeveloperName(){
+    developerName.style.color = '#00fff7';
 }
