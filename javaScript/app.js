c78d918d64331df8af94ce3df99c5ac0530f5c30
origuy <92257911+origuy@users.noreply.github.com>
2022-01-18 18:35:32 +0200

    Update app.js

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,21 +1,34 @@
-const overlayTapDisplay = document.getElementById('overlayHover');
+const overlayTapDisplay = document.getElementById('overlayHoverContainer');
 const mainHeader = document.getElementById('mainHeader');
 const developerName = document.getElementById('developerName');
+let showOverlayFlag = false;
+let changeColorHeaderFlag = false;
+let changeColorDeveloperNameFlag = false;
+
 
 overlayTapDisplay.addEventListener('touchstart', showOverlay);
 mainHeader.addEventListener('touchstart', changeColorHeader);
 developerName.addEventListener('touchstart', changeColorDeveloperName);
 
 function showOverlay(){
-    if(overlayTapDisplay.style.opacity == '1'){
-        overlayTapDisplay.style.opacity = '0';
-    }else{
+    if(showOverlayFlag === false){
         overlayTapDisplay.style.opacity = '1';
+        showOverlayFlag = true
+    }else{
+        overlayTapDisplay.style.opacity = '0';
     }
 }
 function changeColorHeader(){
-    mainHeader.style.color = '#00fff7';
+    if(changeColorHeaderFlag === false){
+        mainHeader.style.color = '#00fff7';
+    }else{
+        mainHeader.style.color = '#fff';
+    }
 }
 function changeColorDeveloperName(){
-    developerName.style.color = '#00fff7';
+    if(changeColorDeveloperNameFlag === false){
+        developerName.style.color = '#00fff7';
+    }else{
+        developerName.style.color = '#fff';
+    }
 }
